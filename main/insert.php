da01edb361abd1bad4e53f4018f775e20465cd85
0N9 <41815943+0N9@users.noreply.github.com>
2018-08-23 23:12:10 +0800

    Create insert.php

---
+++ main/insert.php
@@ -0,0 +1,8 @@
+<?php
+$con = mysqli_connect("127.0.0.1", "id6717512_jesus", "Fvckyou123fuckerss", "my_db");
+
+$chat = $[POST]['message-form__input'];
+
+$sql = "INSERT INTO chat (chat) VALUES ('chat');
+
+?>
