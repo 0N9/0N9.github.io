6ddecb540c3f607de559884025351291f7a81930
0N9 <41815943+0N9@users.noreply.github.com>
2018-09-12 22:43:41 +0100

    Create cardimage.js

---
+++ cardimage.js
@@ -0,0 +1,28 @@
+var fileUpload = document.getElementById('fileUpload');
+var canvas  = document.getElementById('canvas');
+var ctx = canvas.getContext("2d");
+
+function readImage() {
+    if ( this.files && this.files[0] ) {
+        var FR= new FileReader();
+        FR.onload = function(e) {
+           var img = new Image();
+           img.src = e.target.result;
+           img.onload = function() {
+             ctx.drawImage(img, 0, 0, 512, 512);
+           };
+        };       
+        FR.readAsDataURL( this.files[0] );
+    }
+}
+
+fileUpload.onchange = readImage;
+
+canvas.onclick = function(e) {
+  var x = e.offsetX;
+  var y = e.offsetY;
+  ctx.beginPath();
+  ctx.fillStyle = 'black';
+  ctx.arc(x, y, 5, 0, Math.PI * 2);
+  ctx.fill();
+};
